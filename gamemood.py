73a2ed0aa4234e5e7e2f820f129b00f903a18586
adolfo-stack <60071128+adolfo-stack@users.noreply.github.com>
2021-12-30 22:17:37 +0100

    Add files via upload

---
+++ gamemood.py
@@ -0,0 +1,21 @@
+
+question = input ("How do you feel today?")
+
+answer = str(question)
+
+if answer == "Happy":
+    print("Is great to see you happy")
+
+elif answer == "Angry":
+    print("Why are you angry, you should be happy")
+
+elif answer == "Bored":
+    print("That has solution, start doing something interesting")
+
+else:
+    print("I don´t understand you,sorry")
+
+
+
+
+
